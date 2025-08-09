4758c22aac77514ce936cbd4465cf6123c413e7b
Gaby <gabbiebrabo@gmail.com>
2025-08-08 21:07:21 -0400

    refactor: remove commented-out code and improve readability in user creation and login test

--- app/repo/users.py
+++ app/repo/users.py
@@ -3,14 +3,11 @@ from sqlalchemy.orm import Session
 from app.models.users import Users
 
 def create_user(db: Session, name: str, password: str) -> Users:
-    try: 
         user = Users(name=name,password=password)
         db.add(user)
-        db.commit() 
-        db.refresh(user) # Refresh the instance to get the updated state from the database
+        db.commit()
+        db.refresh(user)
         return user
-    except Exception as e: 
-        print(e, "erro no repo")
 
 def get_user_by_id(db: Session, user_id: int) -> Users | None:
     return db.query(Users).filter(Users.id == user_id).first()
