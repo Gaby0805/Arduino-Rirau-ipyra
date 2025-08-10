2e3ad4175a43472cfc47e354523dddc46f8f790c
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-10 18:22:23 -0400

    feat: update jwt file and register route in main

--- app/infra/jwt.py
+++ app/infra/jwt.py
@@ -3,10 +3,11 @@ from typing import Annotated
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer
-from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, status
 from datetime import timedelta, datetime, timezone
 import jwt
+from jwt.exceptions import InvalidTokenError
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
@@ -42,7 +43,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto" )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-app = FastAPI()
+router = APIRouter(prefix="/token", tags=["auth"])
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -73,4 +74,41 @@ def create_access_token(data: dict, expires_delta:timedelta | None = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-#async def get_current_user(token: Ann)
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate":"Bearer"}
+    )
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+
+
+@router.post("")
+async def login_for_acess_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user:User = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    acesss_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.name}, expires_delta=acesss_token_expires
+    )
+    return Token(acess_token = access_token, token_type="bearer")
--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.api.routes import auth 
+from app.infra import jwt 
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(jwt.router)
