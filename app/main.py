131b19c4c036ee21f4d0768d0eeb4aeca2e53c78
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-10 19:44:45 -0400

    feat: test init database on main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from app.api.routes import auth 
 from app.infra import jwt 
+from app.core.database import Base,engine
+
+
+Base.metadata.create_all(bind=engine) # obrigado para criar as tabelas antes de usar o banco de dados
+
 app = FastAPI()
 
 app.include_router(auth.router)
