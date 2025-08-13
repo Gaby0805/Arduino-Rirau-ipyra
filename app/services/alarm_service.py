2dbf775955807546ba6088a026b26a0135c0338d
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-13 10:52:29 -0400

    feat: create crud for alarms

---
+++ app/services/alarm_service.py
@@ -0,0 +1,32 @@
+from datetime import time as tm
+from typing import List
+from app.models.alarms import Alarms
+from app.repo.alarms import AlarmsRepository
+from app.exceptions.alarm_not_found_exception import AlarmNotFoundException
+
+
+class AlarmsService:
+    def __init__(self):
+        self.repository = AlarmsRepository()
+
+    def create_alarm(self, label: str, time: tm, is_active: bool = True) -> Alarms:
+        return self.repository.create_alarm(label, time, is_active)
+
+    def get_alarm_by_id(self, alarm_id: int) -> Alarms:
+        alarm = self.repository.get_alarm_by_id(alarm_id)
+        if alarm is None:
+            raise AlarmNotFoundException()
+        return alarm
+
+    def get_all_alarms(self) -> List[Alarms]:
+        return self.repository.get_all_alarms()
+
+    def delete_alarm(self, alarm_id: int) -> None:
+        alarm = self.get_alarm_by_id(alarm_id)
+        self.repository.delete_alarm(alarm)
+
+    def get_alarm_by_label(self, label: str) -> Alarms:
+        alarm = self.repository.get_alarm_by_label(label)
+        if alarm is None:
+            raise AlarmNotFoundException()
+        return alarm
