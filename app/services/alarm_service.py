2f5d2d5c3b15bc67625a1ae1b6d439ea0b679390
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-13 14:36:40 -0400

    feat: make easier to create a alarm

--- app/services/alarm_service.py
+++ app/services/alarm_service.py
@@ -3,16 +3,31 @@ from typing import List
 from app.models.alarms import Alarms
 from app.repo.alarms import AlarmsRepository
 from app.exceptions.alarm_not_found_exception import AlarmNotFoundException
-
+from pydantic import conlist
 from app.services.user_service import UserService
+from app.services.alarms_days_service import AlarmsDaysService
 class AlarmsService:
     def __init__(self):
         self.repository = AlarmsRepository()
         self.user_service = UserService()
+        self.alarms_days_service = AlarmsDaysService()
 
-    def create_alarm(self, label: str, time: tm, user_name:str,is_active: bool = True) -> Alarms:
+    def create_alarm(self, label: str, time: tm,days:List[int], user_name:str, is_active: bool = True) -> dict:
         user_id = self.user_service.get_user_by_name(user_name).id
-        return self.repository.create_alarm(label, time,user_id, is_active)
+        alarm =  self.repository.create_alarm(label, time,user_id, is_active)
+        #aqui nao é o melhor jeito de se fazer, o certo é escrever uma query personalizada
+        for day in days:
+            self.alarms_days_service.create_alarm_day(alarm.id, day)
+            
+        return  {
+        "id": alarm.id,
+        "label": alarm.label,
+        "time": alarm.time,
+        "user_id": alarm.user_id,
+        "is_active": alarm.is_active,
+        "days": days  
+    }
+        
 
     def get_alarm_by_id(self, alarm_id: int) -> Alarms:
         alarm = self.repository.get_alarm_by_id(alarm_id)
