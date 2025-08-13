75a9383dd47a312379e85240f3dfbdb3d32f3001
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-13 13:54:12 -0400

    feat: add routes to amin

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.routes import auth 
+from app.api.routes import auth,alarms_days,alarms
 from app.infra import jwt 
 from app.core.database import Base,engine
 from dotenv import load_dotenv
@@ -8,4 +8,6 @@ Base.metadata.create_all(bind=engine) # obrigado para criar as tabelas antes de 
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(alarms.router)
+app.include_router(alarms_days.router)
 
